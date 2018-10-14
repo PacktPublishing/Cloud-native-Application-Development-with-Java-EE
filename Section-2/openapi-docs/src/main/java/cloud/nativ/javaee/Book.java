c88b20da0e1d93caac1293208d986f36c5052d9d
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-10-14 14:46:05 +0200

    Prepared initial branch for Video 2.4. Removed solution code.

--- Section-2/openapi-docs/src/main/java/cloud/nativ/javaee/Book.java
+++ Section-2/openapi-docs/src/main/java/cloud/nativ/javaee/Book.java
@@ -1,18 +1,13 @@
 package cloud.nativ.javaee;
 
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-
 import javax.json.bind.annotation.JsonbCreator;
 import javax.json.bind.annotation.JsonbProperty;
 
-@Schema(name = "Book", description = "POJO that represents a book.")
 public class Book {
 
-    @Schema(required = true, example = "978-0345391803")
     @JsonbProperty("isbn-13")
     public String isbn13;
 
-    @Schema(required = true, example = "The Hitchhiker's Guide to the Galaxy")
     public String title;
 
     @JsonbCreator
--- Section-2/openapi-docs/src/main/java/cloud/nativ/javaee/BookResource.java
+++ Section-2/openapi-docs/src/main/java/cloud/nativ/javaee/BookResource.java
@@ -1,12 +1,5 @@
 package cloud.nativ.javaee;
 
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.validation.constraints.NotNull;
@@ -32,27 +25,17 @@ public class BookResource {
 
     @PostConstruct
     @HEAD
-    @Operation(summary = "Initialize and reset.", description = "Reset the internal bookshelf to default.")
-    @APIResponse(responseCode = "204", description = "No content.")
     public void initialize() {
         bookshelf.put("978-0345391803",
                 new Book("978-0345391803", "The Hitchhiker's Guide to the Galaxy"));
     }
 
     @GET
-    @Operation(summary = "Get all books.", description = "Retrieves the list of all books.")
-    @APIResponse(responseCode = "200", description = "The list of all books.",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(type = SchemaType.ARRAY, implementation = Book.class)))
     public Response books() {
         return Response.ok(bookshelf.values()).build();
     }
 
     @POST
-    @Operation(summary = "Create a new book.", description = "Creates a new book.")
-    @APIResponse(responseCode = "201", description = "The book has been created.")
-    @RequestBody(name = "book", required = true,
-            content = @Content(mediaType = "application/json", schema = @Schema(ref = "Book")))
     public Response create(@NotNull Book book) {
         bookshelf.putIfAbsent(book.isbn13, book);
         URI location = UriBuilder.fromResource(BookResource.class)
@@ -64,10 +47,6 @@ public class BookResource {
 
     @GET
     @Path("/{isbn}")
-    @Operation(summary = "Get book.", description = "Get a book by ISBN-13.")
-    @APIResponse(name = "ok", responseCode = "200", description = "The list of all books.",
-            content = @Content(mediaType = "application/json", schema = @Schema(ref = "Book")))
-    @APIResponse(responseCode = "404", description = "The book was not found.")
     public Response get(@PathParam("isbn") String isbn) {
         Book book = bookshelf.get(isbn);
         if (book != null) {
@@ -79,11 +58,6 @@ public class BookResource {
 
     @PUT
     @Path("/{isbn}")
-    @Operation(summary = "Update book.", description = "Update book identified by ISBN-13.")
-    @APIResponse(responseCode = "200", description = "Update successful.")
-    @APIResponse(responseCode = "400", description = "The ISBN did not match or request was invalid.")
-    @RequestBody(name = "book", required = true,
-            content = @Content(mediaType = "application/json", schema = @Schema(ref = "Book")))
     public Response update(@PathParam("isbn") String isbn, @NotNull Book book) {
         if (!Objects.equals(isbn, book.isbn13)) {
             throw new BadRequestException("ISBN must match path parameter.");
@@ -94,9 +68,6 @@ public class BookResource {
 
     @DELETE
     @Path("/{isbn}")
-    @Operation(summary = "Delete book.", description = "Delete a book identified by ISBN-13.")
-    @APIResponse(responseCode = "204", description = "No content.")
-    @APIResponse(responseCode = "404", description = "The book was not found.")
     public Response delete(@PathParam("isbn") String isbn) {
         Book book = bookshelf.remove(isbn);
         if (book != null) {
