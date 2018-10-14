b0d2c653f0448b898e049e847eaf6ac2d8f27f51
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-10-14 16:15:59 +0200

    Prepared initial branch for Video 3.1. Removed solution.

--- Section-3/config-service/src/main/java/cloud/nativ/javaee/JAXRSConfiguration.java
+++ Section-3/config-service/src/main/java/cloud/nativ/javaee/JAXRSConfiguration.java
@@ -1,13 +1,11 @@
 package cloud.nativ.javaee;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  * Configures a JAX-RS endpoint.
  */
-@ApplicationScoped
 @ApplicationPath("api")
 public class JAXRSConfiguration extends Application {
 }
