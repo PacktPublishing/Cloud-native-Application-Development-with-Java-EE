3b140d0e1cf2149e7a6314437a262e5e1391c86a
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-11-07 00:12:03 +0100

    Prepared branch for Video 6.1

--- Section-6/security-service/src/main/java/cloud/nativ/javaee/JAXRSConfiguration.java
+++ Section-6/security-service/src/main/java/cloud/nativ/javaee/JAXRSConfiguration.java
@@ -1,17 +1,11 @@
 package cloud.nativ.javaee;
 
-import javax.annotation.security.DeclareRoles;
-import javax.enterprise.context.ApplicationScoped;
-import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  * Configures a JAX-RS endpoint.
  */
-@ApplicationScoped
 @ApplicationPath("api")
-@BasicAuthenticationMechanismDefinition(realmName = "security-service")
-@DeclareRoles({"admin", "user"})
 public class JAXRSConfiguration extends Application {
 }
